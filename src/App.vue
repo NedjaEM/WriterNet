0f457ff48bec22a0f9b69e65d6b04396bb55b46f
NedjaEM <nadiaelmouldi@gmail.com>
2022-04-04 02:25:10 -0400

    adding all to master

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <!-- <v-app-bar app color="black" dark> -->
+    <v-main>
+      <AllYears />
+    </v-main>
+    <!-- </v-app-bar> -->
+  </v-app>
+</template>
+
+<script>
+// import NetworkGraph from "./components/NetworkGraph";
+import AllYears from "./components/AllYears.vue";
+import NetworkGraph from './components/NetworkGraph.vue';
+export default {
+  name: "App",
+
+  components: {
+    // NetworkGraph,
+    AllYears,
+    NetworkGraph,
+  },
+
+  data: () => ({}),
+};
+</script>
+
+<style>
+v-app {
+  background-color: #fff7ae;
+}
+</style>
